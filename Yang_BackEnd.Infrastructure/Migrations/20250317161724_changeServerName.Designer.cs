aa7883bb954f34eba5e02e47262fc77bd63baee6
Dương Văn Trí <id.dvantri911@gmail.com>
2025-03-18 22:45:04 +0700

    Fix: ChangeDb

--- Yang_BackEnd.Infrastructure/Migrations/20250317161724_changeServerName.Designer.cs
+++ Yang_BackEnd.Infrastructure/Migrations/20250317161724_changeServerName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Yang_BackEnd.Infrastructure;
 
@@ -11,9 +12,11 @@ using Yang_BackEnd.Infrastructure;
 namespace Yang_BackEnd.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250317161724_changeServerName")]
+    partial class changeServerName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Yang_BackEnd.Infrastructure/Migrations/20250317161724_changeServerName.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yang_BackEnd.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeServerName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_productVariants_products_ProductId",
+                table: "productVariants");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_productVariants",
+                table: "productVariants");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_products",
+                table: "products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_categories",
+                table: "categories");
+
+            migrationBuilder.RenameTable(
+                name: "productVariants",
+                newName: "ProductVariants");
+
+            migrationBuilder.RenameTable(
+                name: "products",
+                newName: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "categories",
+                newName: "Categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_productVariants_ProductId",
+                table: "ProductVariants",
+                newName: "IX_ProductVariants_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_products_CategoryId",
+                table: "Products",
+                newName: "IX_Products_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductVariants",
+                table: "ProductVariants",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductVariants_Products_ProductId",
+                table: "ProductVariants",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductVariants_Products_ProductId",
+                table: "ProductVariants");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductVariants",
+                table: "ProductVariants");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "ProductVariants",
+                newName: "productVariants");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "products");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProductVariants_ProductId",
+                table: "productVariants",
+                newName: "IX_productVariants_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoryId",
+                table: "products",
+                newName: "IX_products_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_productVariants",
+                table: "productVariants",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_products",
+                table: "products",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_categories",
+                table: "categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_productVariants_products_ProductId",
+                table: "productVariants",
+                column: "ProductId",
+                principalTable: "products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
